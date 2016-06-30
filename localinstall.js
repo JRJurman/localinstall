629fa0439d895adca44bc22556bca50ed0b13184
Jesse Jurman <j.r.jurman@gmail.com>
2016-06-30 06:57:11 -0400

    remove commented code

--- localinstall.js
+++ localinstall.js
@@ -3,11 +3,3 @@
 
 var localinstall = require('./index.js').localinstall;
 localinstall();
-// try {
-//   localinstall();
-// }
-// catch (error) {
-//   throw error;
-//   process.exit(1);
-// }
-// process.exit(0);
